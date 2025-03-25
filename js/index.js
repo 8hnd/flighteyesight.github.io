1aa2e046c8380d67894a3341aa540c867e8a57ed
Harry <104951310+8hnd@users.noreply.github.com>
2025-03-25 23:46:47 +0000

    backend (will not be published)

--- js/index.js
+++ js/index.js
@@ -2,3 +2,11 @@ var scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: '#nav-wrapper',
     offset: -100
 })
+function charcountupdate(str) {
+	var lng = str.length;
+	document.getElementById("charcount").innerHTML = lng + ' out of 3000 characters';
+}
+function charcountupdate2(str) {
+	var lng = str.length;
+	document.getElementById("charcount").innerHTML = lng + ' out of 254 characters';
+}
